c8e384ad242e7dd1dbd70b2787cf5703b66440c3
Jeff Escalante <jescalan@users.noreply.github.com>
2019-10-09 13:05:47 -0400

    add jsx prop to test to prevent regression

--- __tests__/fixtures/basic.js
+++ __tests__/fixtures/basic.js
@@ -2,7 +2,7 @@ function main() {
   return (
     <>
       <Foo />
-      <Test something='something else' />
+      <Test something='something else' jsxProp={'wow'} />
       <Test path='files/example.txt' doge='wow' />
     </>
   )
